c001aa275ec07cf2f5fb47268628a214813c7d1a
gugabrandao <guga_br@hotmail.com>
2026-03-29 00:12:51 -0300

    menu blur2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <header className={styles.menu_bg} id="menu_bg">
+    <header className={styles.menu_bg}>
       <a href="#home">
         <img src="/imgs/logo.svg" alt="Logo Cromia" className={styles.logo} id="logo" />
       </a>
