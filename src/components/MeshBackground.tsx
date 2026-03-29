fc49148a68a5b56c0a17ccff2afd69af06bfe8c9
gugabrandao <guga_br@hotmail.com>
2026-03-29 00:50:35 -0300

    3

--- src/components/MeshBackground.tsx
+++ src/components/MeshBackground.tsx
@@ -1,8 +1,10 @@
+import conchas from '../assets/conchas.svg';
 import styles from './MeshBackground.module.css';
 
 export default function MeshBackground() {
   return (
     <div className={styles.cromia_mesh_bg}>
+      <img src={conchas} className={styles.conchas} alt="" />
       <div className={`${styles.blob} ${styles.nude1}`}></div>
       <div className={`${styles.blob} ${styles.amber}`}></div>
       <div className={`${styles.blob} ${styles.nude2}`}></div>
